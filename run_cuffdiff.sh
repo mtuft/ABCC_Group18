f28cde8d60400112cb29ea77565021ae4d5822e1
Michael Tuft <k25049595@kcl.ac.uk>
2025-12-14 22:57:15 +0000

    Update run_cuffdiff.sh

--- run_cuffdiff.sh
+++ run_cuffdiff.sh
@@ -3,10 +3,10 @@
 #SBATCH --output=cuffdiff_UQ_%j.log
 #SBATCH --time=04:00:00
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=16G
+#SBATCH --mem=8G
 #SBATCH --partition=msc_appbio
 
-# 1. Setup Environment (CRITICAL STEP)
+# 1. Setup Environment
 source ~/.bashrc
 conda activate /users/k25049595/.conda/envs/tophat_env
 
@@ -14,9 +14,7 @@ conda activate /users/k25049595/.conda/envs/tophat_env
 GTF="Saccharomyces_cerevisiae.R64-1-1.107.gtf"
 SDRF="E-MTAB-7657.sdrf.txt"
 
-echo "Generating file lists from $SDRF..."
-
-# 3. Auto-generate the lists of BAM files for each time point
+# 3. Generate the lists of BAM files for each time point
 # Group 1: 6 Hours (Lag)
 LIST_6H=$(awk -F'\t' '$30 == "6" {printf "output_directory/" $28 "/accepted_hits.bam,"}' $SDRF | sed 's/,$//')
 
@@ -26,8 +24,7 @@ LIST_14H=$(awk -F'\t' '$30 == "14" {printf "output_directory/" $28 "/accepted_hi
 # Group 3: 26 Hours (Diauxic)
 LIST_26H=$(awk -F'\t' '$30 == "26" {printf "output_directory/" $28 "/accepted_hits.bam,"}' $SDRF | sed 's/,$//')
 
-# 4. Run Cuffdiff (With Upper Quartile Normalization)
-echo "Starting Cuffdiff with Upper Quartile Normalization..."
+# 4. Run Cuffdiff with UQ Normalization
 
 /users/k25049595/.conda/envs/tophat_env/bin/cuffdiff -p 4 \
   -N \
@@ -35,5 +32,3 @@ echo "Starting Cuffdiff with Upper Quartile Normalization..."
   -o cuffdiff_results_UQ \
   $GTF \
   $LIST_6H $LIST_14H $LIST_26H
-
-echo "Cuffdiff analysis complete!"
